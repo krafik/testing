1e62de8e4af57fccafe56bac94521b9a8c022075
krafik <alexeym4l@gmail.com>
2023-08-09 12:16:44 +0300

    получили данные из API. добавили в store, вывели из store в компонент.

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
   <div class="v-catalog">
     
     <vCatalogItem 
-        v-for="product in products" :key="product.article"
+        v-for="product in PRODUCTS" :key="product.article"
         :product_data="product"
         @hello="showArticleFromChild"
     />
@@ -12,81 +12,36 @@
 
 <script>
 import vCatalogItem from './v-catalog-item.vue';
+import {mapActions, mapGetters} from 'vuex';
 export default {
   name: "v-catalog",
   props: {},
   components: { vCatalogItem },
   data() {
     return {
-      "products": [
-        {
-          image: "1.png",
-          name: "Analyst",
-          price: 2100,
-          article: "T1",
-          available: true,
-          category: "it products",
-          quantity: 0,
-          about: "Hi i'm an Analyst! Nice to meet you!"
-        },
-        {
-          image: "2.png",
-          name: "Designer",
-          price: 3150,
-          article: "T2",
-          available: true,
-          category: "it products",
-          quantity: 0,
-          about: "Hey my friend! Nice to meet you!"
-        },
-        {
-          image: "3.png",
-          name: "Marketer",
-          price: 4200,
-          article: "T3",
-          available: false,
-          category: "it products",
-          quantity: 0,
-          about: "Hello! How have you been?"
-        },
-        {
-          image: "4.png",
-          name: "Developer",
-          price: 5300,
-          article: "T4",
-          available: true,
-          category: "it products",
-          quantity: 0,
-          about: "Nice to meet you! What about Vue?"
-        },
-        {
-          image: "5.png",
-          name: "Business Accelerator",
-          price: 6500,
-          article: "T5",
-          available: false,
-          category: "it products",
-          quantity: 0,
-          about: "Hey! We'll solve any problems!"
-        },
-        {
-          image: "6.png",
-          name: "Engineer",
-          price: 8700,
-          article: "T6",
-          available: true,
-          category: "it products",
-          quantity: 0,
-          about: "Hi! I've many solutions!"
-        }
-      ]
+      
     }
   },
+  computed:{
+    ...mapGetters(['PRODUCTS']),
+  },
   methods:{
+    ...mapActions([
+    "GET_PRODUCTS_FROM_API"
+    ]),
     showArticleFromChild(data){
       console.log(data);
+    },
+   
+  },
+  mounted(){
+      this.GET_PRODUCTS_FROM_API()
+      .then((responce)=>{
+        if(responce.data){
+          console.log('data is come');
+        }
+      })
     }
-  }
 };
 </script>
 
